3dda494d1a1336b9ef1863488566f52ad5424efd
jeremysacriz <114373649+jeremysacriz@users.noreply.github.com>
2022-11-24 21:19:00 +1100

    Responsive gallery - 24/11/22

---
+++ js/italygallery.js
@@ -0,0 +1,6 @@
+let elem = elem => document.querySelector(elem)
+let id = id => document.getElementById(id)
+let elemAll = elemAll => document.querySelectorAll(elemAll)
+
+let italyBtn = elemAll('.italy-buttons button')
+let galleria = elemAll('.galleria')
