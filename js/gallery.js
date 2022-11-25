9f7dbc77fe8e6605b29413babb3a6f9098a0a16f
jeremysacriz <114373649+jeremysacriz@users.noreply.github.com>
2022-11-25 20:29:04 +1100

    carousel.js - 25/11/22

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,8 @@
-let elem = elem => document.querySelector(elem)
-let id = id => document.getElementById(id)
-let elemAll = elemAll => document.querySelectorAll(elemAll)
+const elem = elem => document.querySelector(elem)
+const id = id => document.getElementById(id)
+const elemAll = elemAll => document.querySelectorAll(elemAll)
 
-let btns = elemAll('.galleria-buttons button')
+const btns = elemAll('.galleria-buttons button')
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -11,13 +11,49 @@ btns.forEach(btn => {
         delete previousActiveBtn.dataset.active
 
         btn.dataset.active = "true"
-        let newActiveBtn = parentBtn.querySelector('[data-active]')
-        let newBtnIndex = [...parentBtn.children].indexOf(newActiveBtn)
 
+        let previousIndex = [...parentBtn.children].indexOf(previousActiveBtn);
+        let newIndex = [...parentBtn.children].indexOf(btn);
         let parentGallery = elem('.galleria-container')
-        let previousActiveGallery = parentGallery.querySelector('[data-active]')
-        delete previousActiveGallery.dataset.active
 
-        parentGallery.children[newBtnIndex].dataset.active = "true"
+        delete parentGallery.children[previousIndex].dataset.active
+        parentGallery.children[newIndex].dataset.active = "true"
     })
+})
+
+const carousel = elem('.galleria-carousel')
+const carouselPosition = elem('.carousel-position')
+const carouselLength = elem('.carousel-length')
+const galleria = elemAll('.galleria')
+const carouselImg = elem('.carousel-img-container img')
+const leftBtn = elem('.left')
+const rightBtn = elem('.right')
+const galleryImg = elemAll('.gallery-item img')
+
+galleria.forEach(gallery => {
+    let image = gallery.querySelectorAll('.gallery-item img')
+    carouselLength.innerHTML = image.length
+    
+    image.forEach(img => {
+        img.addEventListener('click', () => {
+            let currentIndex = [...image].indexOf(img)
+            carouselPosition.innerHTML = currentIndex + 1
+            console.log(img)
+            
+            image[currentIndex].dataset.active = "true"
+            carousel.classList.add('active')
+            carouselImg.src = image[currentIndex].src
+        })
+    })
+})
+
+let close = elem('.close')
+
+close.addEventListener('click', () => {
+    carousel.classList.remove('active')
+    for (let i = 0; i < galleryImg.length; i++) {
+        if (galleryImg[i].hasAttribute('data-active')) {
+            delete galleryImg[i].dataset.active
+        }
+    }
 })
