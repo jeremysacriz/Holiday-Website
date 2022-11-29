a02cdef2320381b66f12a26234bfe296f5394fe0
jeremysacriz <114373649+jeremysacriz@users.noreply.github.com>
2022-11-29 12:08:57 +1100

    Italy carousel - 29/11/22

--- js/homegallery.js
+++ js/homegallery.js
@@ -2,6 +2,27 @@ const elem = elem => document.querySelector(elem)
 const id = id => document.getElementById(id)
 const elemAll = elemAll => document.querySelectorAll(elemAll)
 
+const carousel = elem('.carousel')
+if (document.body.contains(carousel)) {
+    $(document).ready(function() {
+        $('.carousel').carousel({
+            indicators: true,
+        })
+    })
+}
+
+const italyGalleryItem = elemAll('.carousel .carousel-item')
+
+italyGalleryItem.forEach(item => {
+    item.addEventListener('click', () => {
+        let newIndex = [...italyGalleryItem].indexOf(item)
+        let loaderImg = item.querySelector('img').src
+
+        localStorage.setItem('activeIndex', newIndex)
+        localStorage.setItem('loaderSrc', loaderImg)
+    })
+})
+
 const homeGalleryItem = elemAll('.gallery .gallery-item')
 
 homeGalleryItem.forEach(item => {
