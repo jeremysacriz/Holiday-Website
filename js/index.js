fe4bc7b1b431695b0f0d0b5d055d3e30d59f5274
jeremysacriz <114373649+jeremysacriz@users.noreply.github.com>
2022-12-01 11:04:25 +1100

    index.html - 1/12/22

--- js/index.js
+++ js/index.js
@@ -2,3 +2,26 @@
 let elem = elem => document.querySelector(elem)
 let elemAll = all => document.querySelector(all)
 let id = id => document.getElementById(id)
+
+const video = elem('.index-video')
+const videoOverlay = elem('.home-overlay')
+const layers = elem('.layers')
+const layersClear = elem('.layers-clear')
+const audio = elem('.audio')
+const mute = elem('.mute')
+
+layers.addEventListener('click', () => {
+    videoOverlay.classList.toggle('active')
+    layers.classList.add('active')
+    layersClear.classList.add('active')
+
+    audio.classList.add('active')
+    mute.classList.add('active')
+    video.play()
+})
+
+layersClear.addEventListener('click', () => {
+    layersClear.classList.remove('active')
+    layers.classList.remove('active')
+    videoOverlay.classList.remove('active')
+})
